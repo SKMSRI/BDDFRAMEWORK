ca5bf6a2d98153bcccf04e2b5feb518d7fd174d5
SAURABH KUMAR SRIVASTAVA <78096853+SaurabhKumarSrivastava24@users.noreply.github.com>
2021-08-11 12:44:48 +0530

    aded

---
+++ BDDFRAMEWORK/src/test/java/StepDefination/stepdefiniations.java
@@ -0,0 +1,27 @@
+package StepDefination;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepdefiniations {
+	
+	@Given("I have a search Field in AED page")
+	public void i_have_a_search_field_in_aed_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click on Search box and enter the QR name")
+	public void click_on_search_box_and_enter_the_qr_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The QR should show in page")
+	public void the_qr_should_show_in_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
